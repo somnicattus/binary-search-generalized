44afdbb9aa414fa0f547859a76df2d526c78b98d
somnicattus <somnicattus@gmail.com>
2025-08-23 16:14:56 +0900

    chore: build

--- dist/index.js
+++ dist/index.js
@@ -75,6 +75,8 @@ const midpointDouble = (value1, value2) => {
     const diff = Math.abs(exponent1 - exponent2);
     if (diff <= 1)
         return value1 / 2 + value2 / 2;
+    if (value1 !== 0 && value2 !== 0 && value1 > 0 !== value2 > 0)
+        return 0;
     return (exponent1 > exponent2 ? value1 : value2) * 2 ** -(diff / 2);
 };
 const shouldContinueDouble = (value1, value2) => {
